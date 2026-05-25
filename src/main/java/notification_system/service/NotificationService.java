9ea540599c10f221ff6839d08d1e67fcc3049f9a
이민경 <leeminkyung@imingyeong-ui-MacBookAir.local>
2026-05-25 22:34:26 +0900

    Block repeated failure users before saving notifications

--- src/main/java/notification_system/service/NotificationService.java
+++ src/main/java/notification_system/service/NotificationService.java
@@ -1,10 +1,12 @@
 package notification_system.service;
 
 import notification_system.domain.Notification;
+import notification_system.domain.NotificationStatus;
 import notification_system.repository.NotificationRepository;
+
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-import notification_system.domain.NotificationStatus;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class NotificationService {
@@ -15,27 +17,8 @@ public class NotificationService {
         this.repository = repository;
     }
 
+    @Transactional
     public void send(Notification notification) {
-
-        if (repository.findByReceiverIdAndEventId(
-                notification.getReceiverId(),
-                notification.getEventId()
-        ).isPresent()) {
-
-            System.out.println("중복 알림 요청 - 저장하지 않음");
-            return;
-        }
-
-        try {
-            repository.save(notification);
-
-            System.out.println("알림 요청 접수");
-            System.out.println("REQUESTED 저장 완료");
-
-        } catch (DataIntegrityViolationException e) {
-
-            System.out.println("동시 중복 알림 요청 - 저장하지 않음");
-        }
 
         long failedCount =
                 repository.countByReceiverIdAndStatus(
@@ -44,8 +27,45 @@ public class NotificationService {
                 );
 
         if (failedCount >= 10) {
-            System.out.println("반복 실패 사용자 - 알림 요청 차단");
+
+            System.out.println(
+                    "반복 실패 사용자 - 알림 요청 차단"
+            );
+
             return;
+        }
+
+        if (repository.findByReceiverIdAndEventId(
+                notification.getReceiverId(),
+                notification.getEventId()
+        ).isPresent()) {
+
+            System.out.println(
+                    "중복 알림 요청 - 저장하지 않음"
+            );
+
+            return;
+        }
+
+        try {
+
+            repository.save(
+                    notification
+            );
+
+            System.out.println(
+                    "알림 요청 접수"
+            );
+
+            System.out.println(
+                    "REQUESTED 저장 완료"
+            );
+
+        } catch (DataIntegrityViolationException e) {
+
+            System.out.println(
+                    "동시 중복 알림 요청 - 저장하지 않음"
+            );
         }
     }
 }
